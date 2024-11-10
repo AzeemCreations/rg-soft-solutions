ce1bc6f479a8c81b64744994dbc68260448b20ae
AzeemCreations <ajeemattar99@gmail.com>
2024-11-10 19:39:49 +0530

    Changed Description

--- src/app/Components/Banner.js
+++ src/app/Components/Banner.js
@@ -14,7 +14,7 @@ export default function Banner() {
                                     <h2>Crafting Your <em>Digital</em><span> Future</span></h2>
                                     <p>At RG Software Solutions, we specialize in transforming your ideas into reality with our expertise in web development and app development. Whether it’s building robust eCommerce platforms, intuitive hotel management apps, dynamic exam portals, or comprehensive billing and inventory software, our solutions are designed to meet the unique needs of your business. Trust us to elevate your digital presence and streamline your operations with innovative, reliable, and cutting-edge technology.</p>
                                     <fieldset>
-                                        <button style={{ backgroundColor: "#1D3BA9", padding: "10px 40px", color: 'white', borderRadius: '35px', border: 'none' }}>Explore More</button>
+                                        <button href="#about" style={{ backgroundColor: "#1D3BA9", padding: "10px 40px", color: 'white', borderRadius: '35px', border: 'none' }}>Explore More</button>
                                     </fieldset>
                                 </div>
                             </div>
